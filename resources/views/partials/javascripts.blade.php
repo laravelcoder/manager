3776530b044f536306393898874d2c21e6db5970
WeCodeLaravel <wecodelaravel@gmail.com>
2018-08-17 17:51:37 -0600

    update files added

--- resources/views/partials/javascripts.blade.php
+++ resources/views/partials/javascripts.blade.php
@@ -19,24 +19,82 @@
 <script src="https://cdn.datatables.net/buttons/1.2.4/js/buttons.colVis.min.js"></script>
 <script src="https://cdn.datatables.net/select/1.2.0/js/dataTables.select.min.js"></script>
 <script src="https://code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>
-<script src="{{ url('adminlte/js') }}/bootstrap.min.js"></script>
-<script src="{{ url('adminlte/js') }}/select2.full.min.js"></script>
-<script src="{{ url('adminlte/js') }}/main.js"></script>
+<script src="{{ url('adminlte/js/bootstrap.min.js') }}"></script>
+<script src="{{ url('adminlte/js/select2.full.min.js') }}"></script>
+<script src="{{ url('adminlte/js/fullHeight.js') }}"></script>
+
+<script src="{{ url('adminlte/js/main.js') }}"></script>
 
 <script src="{{ url('adminlte/plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
 <script src="{{ url('adminlte/plugins/fastclick/fastclick.js') }}"></script>
+ 
 <script src="{{ url('adminlte/js/app.min.js') }}"></script>
+
+<script src="{{ url('adminlte/js/adminlte.js') }}"></script>
+
 <script>
     window._token = '{{ csrf_token() }}';
 </script>
 <script>
     $.extend(true, $.fn.dataTable.defaults, {
         "language": {
-            "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/English.json"
+            "url": "https://cdn.datatables.net/plug-ins/1.10.15/i18n/{{ config('app.languages')[app()->getLocale()] }}.json"
         }
     });
+    $(document).ready(function() {
+        $('.sidebar').height('100%');
+      });
+    $(document).ready(function() {
+        $('.searchable-field').select2({
+            minimumInputLength: 3,
+            ajax: {
+                url: '{{ route("admin.mega-search") }}',
+                dataType: 'json',
+                type: "GET",
+                delay: 200,
+                data: function (term) {
+                    return {
+                        search: term
+                    };
+                },
+                results: function (data) {
+                    return {
+                        data
+                    };
+                }
+            },
+            escapeMarkup: function (markup) { return markup; },
+            templateResult: formatItem,
+            templateSelection: formatItemSelection,
+            placeholder : 'Search...'
 
-     
+        });
+        function formatItem (item) {
+            if (item.loading) {
+                return 'Searching...';
+            }
+            let markup = "<div class='searchable-link' href='" + item.url + "'>";
+            markup += "<div class='searchable-title'>" + item.model + "</div>";
+            $.each(item.fields, function(key, field) {
+                markup += "<div class='searchable-fields'>" + item.fields_formated[field] + " : " + item[field] + "</div>";
+            });
+            markup += "</div>";
+
+            return markup;
+        }
+
+        function formatItemSelection (item) {
+            if (!item.model) {
+                return 'Search...';
+            }
+            return item.model;
+        }
+        $(document).delegate('.searchable-link', 'click', function() {
+            let url = $(this).attr('href');
+            window.location = url;
+        });
+    });
+
 
 </script>
 
@@ -61,8 +119,55 @@
     });
 </script>
 
- 
+<style>
+    .searchable-title {
+        font-weight: bold;
+    }
+    .searchable-fields {
+        padding-left:5px;
+    }
+    .searchable-link {
+        padding:0 5px 0 5px;
+    }
+    .searchable-link:hover   {
+        cursor: pointer;
+        background: #eaeaea;
+    }
+    .select2-results__option {
+        padding-left: 0px;
+        padding-right: 0px;
+    }
+</style>
 
+<script>
+    $(document).ready(function () {
+        $(".notifications-menu").on('click', function () {
+            if (!$(this).hasClass('open')) {
+                $('.notifications-menu .label-warning').hide();
+                $.get('internal_notifications/read');
+            }
+        });
+    });
+</script>
 
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '275710106580050',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v3.1'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 
 @yield('javascript')
--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -20,11 +20,135 @@
         </a>
 
         
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
 
-        
+                <li class="dropdown notifications-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                        <i class="fa fa-bell-o"></i>
+                        @php($notificationCount = \Auth::user()->internalNotifications()->where('read_at', null)->count())
+                        @if($notificationCount > 0)
+                            <span class="label label-warning">
+                            {{ $notificationCount }}
+                        </span>
+                        @endif
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <div class="slimScrollDiv"
+                                 style="position: relative;">
+                                <ul class="menu notification-menu">
+                                    @if (count($notifications = \Auth::user()->internalNotifications()->get()) > 0)
+                                        @foreach($notifications as $notification)
+                                            <li class="notification-link {{ $notification->pivot->read_at === null ? "unread" : false }}">
+                                                <a href="{{ $notification->link ? $notification->link : "#" }}"
+                                                   class="{{ $notification->link ? 'is-link' : false }}">
+                                                    {{ $notification->text }}
+                                                </a>
+                                            </li>
+                                        @endforeach
+                                    @else
+                                        <li class="notification-link" style="text-align:center;">
+                                            No notifications
+                                        </li>
+                                    @endif
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+
+                <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <li class="dropdown languages-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                        {{ strtoupper(\App::getLocale()) }}
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="header"></li>
+                        <ul class="menu language-menu">
+                            @foreach(config('app.languages') as $short => $title)
+                                <li class="language-link">
+                                    <a href="{{ route('admin.language', $short) }}">
+                                        {{ $title }} ({{ strtoupper($short) }})
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                        <li class="footer"></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
 
     </nav>
 </header>
 
 
+<style>
+    .slimScrollDiv {
+        width: auto !important;
+        height:auto !important;
+    }
+
+    .notification-menu {
+        width: auto !important;
+        list-style-type: none;
+        padding: 5px;
+        max-width: 300px;
+        height:auto !important;
+    }
+
+    .notification-link {
+        width: auto;
+    }
+
+    .notification-link a {
+        white-space: normal !important;
+    }
+
+    .unread a {
+        font-weight: bold !important;
+    }
+
+    .is-link {
+        color: #5b9bd1 !important;
+    }
+</style><style>
+    .slimScrollDiv {
+        width: auto !important;
+        height:auto !important;
+    }
+
+    .language-menu {
+        width: auto !important;
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+        max-width: 300px;
+        height:auto !important;
+        max-height: 500px !important;
+    }
+
+    .language-link {
+        width: auto;
+    }
+
+    .language-link a {
+        display: block;
+        width: 100%;
+        white-space: normal !important;
+        padding: 5px;
+    }
+    .language-link a:hover {
+        color: #389ad2;
+        background: #f9f9f9;
+    }
+</style>
 
