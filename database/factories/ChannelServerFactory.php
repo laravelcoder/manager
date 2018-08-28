9a5ad177726c5eccd55713e16ee0e1a94fad098f
WeCodeLaravel <wecodelaravel@gmail.com>
2018-08-28 16:56:34 -0600

    Apply fixes from StyleCI (#43)

--- database/factories/ChannelServerFactory.php
+++ database/factories/ChannelServerFactory.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
 $factory->define(App\ChannelServer::class, function (Faker\Generator $faker) {
     return [
-        "name" => $faker->name,
-        "cs_host" => $faker->name,
+        'name' => $faker->name,
+        'cs_host' => $faker->name,
     ];
 });
