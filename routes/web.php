eafea3b394b3911f093893a378212f60144cc8a6
WeCodeLaravel <wecodelaravel@gmail.com>
2018-08-30 12:00:19 -0600

    added docs to new manager
    Added technical documentation for the site and api

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,42 @@
 <?php
 
 declare(strict_types=1);
+
+// Route::middleware('auth')->group(function () {
+    Route::get('/r', function () {
+        function philsroutes() {
+            $routeCollection = Route::getRoutes();
+            echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">';
+            echo "<div class='container'><div class='col-md-12'><table class='table table-striped' style='width:100%'>";
+            echo '<tr>';
+            echo "<td width='10%'><h4>HTTP Method</h4></td>";
+            echo "<td width='30%'><h4>URL</h4></td>";
+            echo "<td width='30%'><h4>Route</h4></td>";
+            echo "<td width='30%'><h4>Corresponding Action</h4></td>";
+            echo '</tr>';
+
+            foreach ($routeCollection as $value) {
+                echo '<tr>';
+                echo '<td>' . $value->methods()[0] . '</td>';
+                echo "<td><a href='" . $value->uri() . "' target='_blank'>" . $value->uri() . '</a> </td>';
+                echo '<td>' . $value->getName() . '</td>';
+                echo '<td>' . $value->getActionName() . '</td>';
+                echo '</tr>';
+            }
+
+            echo '</table></div></div>';
+            echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>';
+        }
+
+        return philsroutes();
+    });
+// });
+
+
+
+
+
+
 Route::get('/', function () {
     return redirect('/admin/home');
 });
@@ -22,6 +58,9 @@ $this->post('password/reset', 'Auth\ResetPasswordController@reset')->name('auth.
 
 Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], function (): void {
     Route::get('/home', 'HomeController@index');
+    Route::get('logs', ['uses' => '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index', 'as' => 'admin.logs']);
+    Route::get('docs', function () {    return redirect('/docs'); });
+    
 
     Route::resource('channel_servers', 'Admin\ChannelServersController');
     Route::post('channel_servers_mass_destroy', ['uses' => 'Admin\ChannelServersController@massDestroy', 'as' => 'channel_servers.mass_destroy']);
